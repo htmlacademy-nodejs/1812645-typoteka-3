ab799a6344b0ed0badbe16611faed39d849e1e70
lgmarch <lmarch@yandex.ru>
2022-03-19 02:46:10 +0300

    Создание сервиса по обработке ввода пользователя: service.js.

--- src/service/service.js
+++ src/service/service.js
@@ -1,3 +1,22 @@
 'use strict';
 
-console.log(`Hello, world!`);
+const {getArrayOfArgv} = require(`../service/utils/utils`);
+const {Cli} = require(`../service/cli`);
+const {DEFAULT_COMMAND, USER_ARGV_INDEX, ExitCode} = require(`../service/const/constants`);
+
+let arrayOfUserCommand = getArrayOfArgv(process.argv.slice(USER_ARGV_INDEX));
+
+if (arrayOfUserCommand.length === 0) {
+  arrayOfUserCommand = [{[DEFAULT_COMMAND]: null}];
+}
+
+arrayOfUserCommand.forEach((userCommand) => {
+  const command = Object.keys(userCommand)[0];
+  const value = userCommand[command];
+
+  const result = Cli[command].run(value);
+  if (result) {
+    process.exit(ExitCode.error);
+  }
+});
+process.exit(ExitCode.success);
