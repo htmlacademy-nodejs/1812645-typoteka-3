101bc5bdc60cc18f6b80daaea9dbb078229d8bc9
lgmarch <lmarch@yandex.ru>
2022-08-15 01:26:13 +0300

    Подключит сервис Категории к Базе Данных. Отобразит на страничке категории.

--- src/service/api/index.js
+++ src/service/api/index.js
@@ -8,6 +8,9 @@ const search = require(`../api/search`);
 const {MOCK_FILE_NAME} = require(`../../constants`);
 const getMockData = require(`../lib/get-mock-data`);
 
+const sequelize = require(`../lib/sequelize`);
+const defineModels = require(`../models`);
+
 const {
   ArticleService,
   CategoryService,
@@ -16,11 +19,13 @@ const {
 
 const app = new Router();
 
+defineModels(sequelize);
+
 (async () => {
   const data = await getMockData(MOCK_FILE_NAME);
   const mockData = JSON.parse(data);
 
-  categories(app, new CategoryService(mockData));
+  categories(app, new CategoryService(sequelize));
   articles(app, new ArticleService(mockData));
   search(app, new SearchService(mockData));
 })();
