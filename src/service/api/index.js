5853f7b3514989b6d7f9c6219e6945dec929d076
lgmarch <lmarch@yandex.ru>
2022-08-15 02:03:22 +0300

    Подключит сервис Поиска к Базе Данных.

--- src/service/api/index.js
+++ src/service/api/index.js
@@ -5,9 +5,6 @@ const categories = require(`../api/categories`);
 const articles = require(`../api/articles`);
 const search = require(`../api/search`);
 
-const {MOCK_FILE_NAME} = require(`../../constants`);
-const getMockData = require(`../lib/get-mock-data`);
-
 const sequelize = require(`../lib/sequelize`);
 const defineModels = require(`../models`);
 
@@ -22,12 +19,9 @@ const app = new Router();
 defineModels(sequelize);
 
 (async () => {
-  const data = await getMockData(MOCK_FILE_NAME);
-  const mockData = JSON.parse(data);
-
   categories(app, new CategoryService(sequelize));
-  articles(app, new ArticleService(mockData));
-  search(app, new SearchService(mockData));
+  articles(app, new ArticleService(sequelize));
+  search(app, new SearchService(sequelize));
 })();
 
 module.exports = app;
