840433e979b7b1c22395d7d230166878d1c80eae
lgmarch <lmarch@yandex.ru>
2022-08-27 16:12:21 +0300

    Закроет всем пользователям, кроме admin доступ к закрытой части сайта.

--- src/express/routes/admin-router.js
+++ src/express/routes/admin-router.js
@@ -1,27 +1,28 @@
 'use strict';
 
 const {Router} = require(`express`);
+const auth = require(`../middlewares/auth`);
 
 const api = require(`../api`).getAPI();
 
 const myCommentsRouter = new Router();
 
 // мои публикации
-myCommentsRouter.get(`/`, async (req, res) => {
+myCommentsRouter.get(`/`, auth, async (req, res) => {
   const articles = await api.getArticles();
 
   res.render(`admin/admin-articles`, {articles});
 });
 
 // комментарии к публикациям
-myCommentsRouter.get(`/comments`, async (req, res) => {
+myCommentsRouter.get(`/comments`, auth, async (req, res) => {
   const articles = await api.getArticles({comments: true});
 
   res.render(`admin/admin-comments`, {articles: articles.slice(0, 3)});
 });
 
 // категории
-myCommentsRouter.get(`/categories`, async (req, res) => {
+myCommentsRouter.get(`/categories`, auth, async (req, res) => {
   const categories = await api.getCategories();
 
   res.render(`admin/admin-categories`, {categories});
